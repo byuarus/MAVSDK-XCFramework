7a6f1872f751adf5bf6a14548bc4933da1e03910
Dmitry Malakhov <dmalakhov@esri.com>
2021-08-24 13:59:51 -0700

    Updated to 0.42.0 release

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   targets: [
     .binaryTarget(name: "mavsdk_server",
-                  url: "https://github.com/byuarus/MAVSDK/releases/download/0.41.1/mavsdk_server.xcframework.zip",
-                  checksum: "40ba6ab936f8042ec784e1f2e00b660a8d74d2c0253e088093e8b1125b17d277")
+                  url: "https://github.com/byuarus/MAVSDK/releases/download/0.42.0/mavsdk_server.xcframework.zip",
+                  checksum: "c7322aff18e6dae45c14b6d03542ccb9faaf477077450a1cb4da44c7bc361929")
   ]
 )
